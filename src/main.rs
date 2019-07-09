99cf90bd1264c9eb17f551351315a63aeea35660
Simon Bergström <simon@menuan.se>
2019-07-09 17:35:27 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use vulkano::buffer::BufferUsage;
+use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
+
+struct MyStruct {
+    a: u32,
+    b: bool,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+        .expect("failed to create instance");
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+
+    let (device, mut queues) = {
+        Device::new(
+            physical,
+            &Features::none(),
+            &DeviceExtensions::none(),
+            [(queue_family, 0.5)].iter().cloned()
+        ).expect("failed to create device")
+    };
+
+    let queue = queues.next().unwrap();
+
+    // let data = 12;
+    let data = MyStruct { a: 5, b: true };
+    
+    let buffer = CpuAccessibleBuffer::from_data(
+        device.clone(), BufferUsage::all(),
+        data
+    ).expect("failed to create buffer");
+
+    let iter = (0 .. 128).map(|_| 5u8);
+    let buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        iter
+    ).unwrap();
+    
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family())
+        .unwrap()
+        .copy_buffer(source.clone(), dest.clone()).unwrap()
+        .build().unwrap();
+}
