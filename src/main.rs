e0f584c8246f5bbecee6fe5b0a1291b87a90ee9e
Simon Bergström <simon@menuan.se>
2019-07-10 11:10:01 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,72 @@
 use std::sync::Arc;
-
 use image::{ImageBuffer, Rgba};
-
-use vulkano::buffer::BufferUsage;
-use vulkano::buffer::CpuAccessibleBuffer;
-
-use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
-
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer, DynamicState};
 use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
-
-use vulkano::device::Device;
-use vulkano::device::DeviceExtensions;
-use vulkano::device::Features;
-
-use vulkano::format::ClearValue;
-use vulkano::format::Format;
-
-use vulkano::image::Dimensions;
-use vulkano::image::StorageImage;
-
-use vulkano::instance::Instance;
-use vulkano::instance::InstanceExtensions;
-use vulkano::instance::PhysicalDevice;
-
-use vulkano::pipeline::ComputePipeline;
-
+use vulkano::device::{Device, DeviceExtensions, Features};
+use vulkano::format::{ClearValue, Format};
+use vulkano::framebuffer::{Framebuffer, Subpass};
+use vulkano::image::{Dimensions, StorageImage};
+use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+use vulkano::pipeline::{ComputePipeline, GraphicsPipeline};
+use vulkano::pipeline::viewport::Viewport;
 use vulkano::sync::GpuFuture;
+use vulkano_win::VkSurfaceBuild;
+use winit::EventsLoop;
+use winit::WindowBuilder;
 
-// #[derive(VulkanoShader)]
-// #[ty = "compute"]
-// #[src = "../assets/shaders/mandelbrot.glsl"]
-// struct cs;
-
-mod cs {
+mod vs {
     vulkano_shaders::shader!{
-        ty: "compute",
+        ty: "vertex",
         src: "
 #version 450
 
-layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
-
-layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
+layout(location = 0) in vec2 position;
 
 void main() {
-  vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
-  vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
-
-  vec2 z = vec2(0.0, 0.0);
-  float i;
-  for (i = 0.0; i < 1.0; i += 0.005) {
-    z = vec2(
-             z.x * z.x - z.y * z.y + c.x,
-             z.y * z.x + z.x * z.y + c.y
-             );
-
-    if (length(z) > 4.0) {
-      break;
-    }
-  }
-
-  vec4 to_write = vec4(vec3(i), 1.0);
-  imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
+    gl_Position = vec4(position, 0.0, 1.0);
 }
 "
     }
 }
 
+mod fs {
+    vulkano_shaders::shader!{
+        ty: "fragment",
+        src: "
+#version 450
+
+layout(location = 0) out vec4 f_color;
+
+void main() {
+    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"
+    }
+}
+
+#[derive(Default, Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+vulkano::impl_vertex!(Vertex, position);
+
 fn main() {
     println!("Hello, world!");
-    let instance = Instance::new(None, &InstanceExtensions::none(), None)
-        .expect("failed to create instance");
+    // let instance = Instance::new(None, &InstanceExtensions::none(), None)
+    //     .expect("failed to create instance");
+
+    let instance = {
+        let extensions = vulkano_win::required_extensions();
+        Instance::new(None, &extensions, None).expect("failed to create Vulkan instance")
+    };
+
     let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
 
+    let mut events_loop = EventsLoop::new();
+    let surface = WindowBuilder::new().build_vk_surface(&events_loop, instance.clone()).unwrap();
+    
     for family in physical.queue_families() {
         println!("Found a queue family with {:?} queue(s)", family.queues_count());
     }
@@ -79,8 +74,6 @@ fn main() {
     let queue_family = physical.queue_families()
         .find(|&q| q.supports_graphics())
         .expect("couldn't find a graphical queue family");
-
-    
 
     let (device, mut queues) = {
         Device::new(
@@ -93,44 +86,133 @@ fn main() {
 
     let queue = queues.next().unwrap();
 
-    let shader = cs::Shader::load(device.clone())
-        .expect("failed to create shader module");
+    // let shader = cs::Shader::load(device.clone())
+    //     .expect("failed to create shader module");
     
-    let compute_pipeline = Arc::new(
-        ComputePipeline::new(device.clone(), &shader.main_entry_point(), &()).expect("failed to create compute pipeline")
-    );
+    // let compute_pipeline = Arc::new(
+    //     ComputePipeline::new(device.clone(), &shader.main_entry_point(), &()).expect("failed to create compute pipeline")
+    // );
     
     let image = StorageImage::new(
         device.clone(),
-        Dimensions::Dim2d { width: 2048, height: 2048 },
+        Dimensions::Dim2d { width: 1024, height: 1024 },
         Format::R8G8B8A8Unorm,
         Some(queue.family())
     ).unwrap();
 
-    let set = Arc::new(
-        PersistentDescriptorSet::start(compute_pipeline.clone(), 0)
-            .add_image(image.clone()).unwrap()
-            .build().unwrap()
-    );
+    // let set = Arc::new(
+    //     PersistentDescriptorSet::start(compute_pipeline.clone(), 0)
+    //         .add_image(image.clone()).unwrap()
+    //         .build().unwrap()
+    // );
     
     let buf = CpuAccessibleBuffer::from_iter(
         device.clone(), BufferUsage::all(),
-        (0 .. 2048 * 2048 * 4).map(|_| 0u8)
+        (0 .. 1024 * 1024 * 4).map(|_| 0u8)
     ).expect("failed to create buffer");
 
-    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family())
+    // let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family())
+    //     .unwrap()
+    //     .dispatch([1024 / 8, 1024 / 8, 1], compute_pipeline.clone(), set.clone(), ()).unwrap()
+    //     .copy_image_to_buffer(image.clone(), buf.clone()).unwrap()
+    //     .build().unwrap();
+
+    // let finished = command_buffer.execute(queue.clone()).unwrap();
+    // finished.then_signal_fence_and_flush().unwrap()
+    //     .wait(None).unwrap();
+
+    // let buffer_content = buf.read().unwrap();
+    // let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    // image.save("image.png").unwrap();
+    
+    // println!("Goodbye, world!");
+        
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        vec![vertex1, vertex2, vertex3].into_iter()
+    ).unwrap();
+
+    let render_pass = Arc::new(vulkano::single_pass_renderpass!(
+        device.clone(),
+        attachments: {
+            color: {
+                load: Clear,
+                store: Store,
+                format: Format::R8G8B8A8Unorm,
+                samples: 1,
+            }
+        },
+        pass: {
+            color: [color],
+            depth_stencil: {}
+        }
+    ).unwrap());
+
+    let framebuffer = Arc::new(
+        Framebuffer::start(render_pass.clone())
+            .add(image.clone()).unwrap()
+            .build().unwrap()
+    );
+    
+    AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap()
+        .begin_render_pass(framebuffer.clone(), false, vec![[0.0, 0.0, 1.0, 1.0].into()])
         .unwrap()
-        .dispatch([2048 / 8, 2048 / 8, 1], compute_pipeline.clone(), set.clone(), ()).unwrap()
-        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap()
-        .build().unwrap();
+        .end_render_pass()
+        .unwrap();
+
+    let vs = vs::Shader::load(device.clone()).expect("failed to create shader module");
+    let fs = fs::Shader::load(device.clone()).expect("failed to create shader module");
+
+    let pipeline = Arc::new(GraphicsPipeline::start()
+        .vertex_input_single_buffer::<Vertex>()
+        .vertex_shader(vs.main_entry_point(), ())
+        .viewports_dynamic_scissors_irrelevant(1)
+        .fragment_shader(fs.main_entry_point(), ())
+        .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+        .build(device.clone())
+        .unwrap());
+
+    let dynamic_state = DynamicState {
+        viewports: Some(vec![Viewport {
+            origin: [0.0, 0.0],
+            dimensions: [1024.0, 1024.0],
+            depth_range: 0.0 .. 1.0,
+        }]),
+        .. DynamicState::none()
+    };
+
+    let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap()
+        .begin_render_pass(framebuffer.clone(), false, vec![[0.0, 0.0, 1.0, 1.0].into()])
+        .unwrap()
+        .draw(pipeline.clone(), &dynamic_state, vertex_buffer.clone(), (), ())
+        .unwrap()
+        .end_render_pass()
+        .unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone())
+        .unwrap()
+        .build()
+        .unwrap();
 
     let finished = command_buffer.execute(queue.clone()).unwrap();
     finished.then_signal_fence_and_flush().unwrap()
         .wait(None).unwrap();
 
     let buffer_content = buf.read().unwrap();
-    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(2048, 2048, &buffer_content[..]).unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
     image.save("image.png").unwrap();
-    
-    println!("Goodbye, world!");
+
+    events_loop.run_forever(|event| {
+        match event {
+            winit::Event::WindowEvent { event: winit::WindowEvent::CloseRequested, .. } => {
+                winit::ControlFlow::Break
+            },
+            _ => winit::ControlFlow::Continue,
+        }
+    });
+    println!("Goodbye for reals, world!");
 }
