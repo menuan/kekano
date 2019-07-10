fb238f3b8dbdddd02bdf48e1f00f7d4df2f1317d
Simon Bergström <simon@menuan.se>
2019-07-10 08:35:22 +0200

    Trying to figure out why nothing happens on OSX

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,11 @@ use vulkano::pipeline::ComputePipeline;
 
 use vulkano::sync::GpuFuture;
 
+// #[derive(VulkanoShader)]
+// #[ty = "compute"]
+// #[src = "../assets/shaders/mandelbrot.glsl"]
+// struct cs;
+
 mod cs {
     vulkano_shaders::shader!{
         ty: "compute",
@@ -38,24 +43,24 @@ layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
 
 void main() {
-    vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
-    vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+  vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+  vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
 
-    vec2 z = vec2(0.0, 0.0);
-    float i;
-    for (i = 0.0; i < 1.0; i += 0.005) {
-        z = vec2(
-            z.x * z.x - z.y * z.y + c.x,
-            z.y * z.x + z.x * z.y + c.y
-        );
+  vec2 z = vec2(0.0, 0.0);
+  float i;
+  for (i = 0.0; i < 1.0; i += 0.005) {
+    z = vec2(
+             z.x * z.x - z.y * z.y + c.x,
+             z.y * z.x + z.x * z.y + c.y
+             );
 
-        if (length(z) > 4.0) {
-            break;
-        }
+    if (length(z) > 4.0) {
+      break;
     }
+  }
 
-    vec4 to_write = vec4(vec3(i), 1.0);
-    imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
+  vec4 to_write = vec4(vec3(i), 1.0);
+  imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
 }
 "
     }
@@ -67,9 +72,15 @@ fn main() {
         .expect("failed to create instance");
     let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
 
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+ 
     let queue_family = physical.queue_families()
         .find(|&q| q.supports_graphics())
         .expect("couldn't find a graphical queue family");
+
+    
 
     let (device, mut queues) = {
         Device::new(
